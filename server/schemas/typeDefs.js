940b52382b55bcd65f26b3ea0356e0d3551f72c2
Sarah Wesley <srwesley722@gmail.com>
2023-11-13 12:30:01 -0600

    Finished making adjustments to and finalizing typeDefs.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,7 @@
 const { gql } = require("apollo-server-express");
 
 // typeDefs
-const typeDefs = sql`
-    type Query {
-        me: User
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
-        removeBook(bookId: String!): User
-    }
+const typeDefs = gql`
     type Book {
         _id: ID
         authors: [String]
@@ -27,6 +18,28 @@ const typeDefs = sql`
         email: String
         bookCount: Int
         savedBooks: [Book]
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
     }
     `;
 
