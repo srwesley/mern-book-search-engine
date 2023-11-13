edd0d2e915a857a2ed5955c7120a2a3cabe42933
Sarah Wesley <srwesley722@gmail.com>
2023-11-13 06:38:27 -0600

    Small, quick edit to a part of the server.js file

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const startApolloServer = async () => {
 
     db.once("open", () => {
         app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
+            console.log(`API server running on http://localhost:${PORT}!`);
             console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
         });
     });
